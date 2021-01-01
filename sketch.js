b6c5276c69e0041ea5382e7ca1875eb17211eeb0
Aayush-Hore <72682032+Aayush-Hore@users.noreply.github.com>
2021-01-01 22:22:54 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+//Create variables here
+var dog,happydog;
+var FoodS,database,FoodStack;
+function preload()
+{
+	//load images here
+}
+ dog = loadImage("dog.js")
+ happydog = loadImage("happydog.js")
+function setup() {
+	createCanvas(500,500);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
